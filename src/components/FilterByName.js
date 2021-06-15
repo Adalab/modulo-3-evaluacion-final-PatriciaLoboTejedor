0c662ba0ee28eb2f017db4800aba754d1c8619eb
PatriciaLoboTejedor <patricialobotejedor@gmail.com>
2021-06-15 21:03:30 +0200

    change ackage json with homepage

--- src/components/FilterByName.js
+++ src/components/FilterByName.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const FilterByName = (props) => {
   return (
-    <>
+    <form>
       <label className="" htmlFor="name">
         Filtrar por nombre:
       </label>
       <input className="" type="text" name="name" id="name" />
-    </>
+    </form>
   );
 };
 
