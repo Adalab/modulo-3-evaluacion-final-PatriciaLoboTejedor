2e42d5ad4d605db8e714354459ddb4c52814d019
PatriciaLoboTejedor <patricialobotejedor@gmail.com>
2021-08-06 11:55:00 +0200

    change api and logo header

--- src/services/apiFetch.js
+++ src/services/apiFetch.js
@@ -1,20 +1,25 @@
 const apiFetch = () => {
-  const urlApi = "https://rickandmortyapi.com/api/character";
+  const urlApi = "https://akabab.github.io/superhero-api/api/all.json";
   return fetch(urlApi)
     .then((response) => response.json())
     .then((data) => {
-      const orderAllCharactersByName = data.results.sort((a, b) =>
+      const orderAllCharactersByName = data.sort((a, b) =>
         a.name.localeCompare(b.name)
       );
+      console.log(data);
       const charactersData = orderAllCharactersByName.map((character) => {
         return {
           id: character.id,
           name: character.name,
-          image: character.image,
-          status: character.status,
-          species: character.species,
-          origin: character.origin.name,
-          episode: character.episode.length,
+          image: character.images.sm,
+          intelligence: character.powerstats.intelligence,
+          strength: character.powerstats.strength,
+          speed: character.powerstats.speed,
+          durability: character.powerstats.durability,
+          power: character.powerstats.intelligence,
+          combat: character.powerstats.intelligence,
+          gender: character.appearance.gender,
+          race: character.appearance.race,
         };
       });
       return charactersData;
